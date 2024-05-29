7d965721fbe75a104f3e9e5eb9a6aef419a559e2
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-05-29 15:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//complete this code
-class Person {}
+// Define the Person class
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+
+    // Getter for age (not specified but useful)
+    get age() {
+        return this._age;
+    }
+}
+
+// Define the Student class, extending Person
+class Student extends Person {
+    study() {
+        console.log(`${this._name} is studying`);
+    }
+}
+
+// Define the Teacher class, extending Person
+class Teacher extends Person {
+    teach() {
+        console.log(`${this._name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
